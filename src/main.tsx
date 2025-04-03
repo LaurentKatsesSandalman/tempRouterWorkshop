2bdc343c2f13b18c813534b6e49038e511a1ef21
LaurentKatsesSandalman <laurent.durup@numericable.fr>
2025-04-03 11:29:57 +0200

    end of workshop

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,35 @@ import App from "./App";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Article from "./pages/Article";
+
+const getWeatherOfTheDay = () => {
+  return "sunny";
+};
+
+const allData = [
+  {
+    id: 1,
+    title: "Lorem Ipsum",
+    content: "Lorem ipsum dolor sit amet",
+  },
+  {
+    id: 2,
+    title: "Schnapsum",
+    content: "Lorem Elsass ipsum Salut bisamme",
+  },
+  {
+    id: 3,
+    title: "Cupcake Ipsum",
+    content: "Tiramisu pastry wafer brownie soufflé",
+  },
+];
+
+type Data = typeof allData[0];
+
+const getSomeData = (id: number) => {
+  return allData.find((article) => article.id === id) as Data | null;
+};
 
 // router creation
 
@@ -14,24 +43,16 @@ const router = createBrowserRouter([
   {
     element: <App />,
     children: [
+      // ...
       {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/about",
-        element: <About />,
+        path: "/articles/:id",
+        element: <Article />,
+        loader: ({ params }) => {
+          const idAsInt = parseInt(params.id ?? "0");
+
+          return getSomeData(idAsInt);
+        },
       },
     ],
   },
 ]);
-
-// rendering
-
-const rootElement = document.getElementById("root");
-
-if (rootElement != null) {
-  ReactDOM.createRoot(rootElement).render(
-    <RouterProvider router={router} />
-  );
-}
